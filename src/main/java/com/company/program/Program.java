18b7cc0aa5f03be729a05166026c22a83a87dd5e
NakiAyame <annguuenhuu9@gmail.com>
2024-04-12 21:49:05 +0700

    CRUD Hibernate

--- src/main/java/com/company/program/Program.java
+++ src/main/java/com/company/program/Program.java
@@ -16,5 +16,28 @@ public class Program {
 		for(Group group: groups) {
 			System.out.println(group);
 		}
+		
+		System.out.println("************* GET GROUP BY ID *************");
+		Group groupByID = groupRepository.getGroupByID(2);
+		System.out.println(groupByID);
+		
+		System.out.println("************* GET GROUP BY NAME *************");
+		Group groupByName = groupRepository.getGroupByName("Java");
+		System.out.println(groupByName);
+		
+		System.out.println("************* CREATE GROUP  *************");
+		Group groupForCreate = new Group();
+		groupForCreate.setName("NodeJS");
+		groupRepository.createGroup(groupForCreate);
+		System.out.println("Sau khi thêm Group: " + groups);
+		
+		System.out.println("************* UPDATE GROUP  *************");
+		groupRepository.updateGroup(2, "Chuyen Java sang Ruby");
+		System.out.println("Sau khi sửa Group 2: " + groups);
+		
+		System.out.println("************* DELETE GROUP  *************");
+		groupRepository.deleteGroup(4);
+		System.out.println("Sau khi xoá Group là 4: " + groups);
+		
 	}
 }
--- src/main/java/com/company/repository/GroupRepository.java
+++ src/main/java/com/company/repository/GroupRepository.java
@@ -2,7 +2,6 @@ package com.company.repository;
 
 import java.util.List;
 
-
 import org.hibernate.Session;
 import org.hibernate.query.Query;
 
@@ -29,6 +28,101 @@ public class GroupRepository {
 
 			// return result
 			return query.list();
+		} finally {
+			session.close();
+		}
+	}
+
+	// CRUD Group
+	public Group getGroupByID(Integer id) {
+		Session session = null;
+
+		try {
+			// get Session
+			session = hibernateutils.openSession();
+
+			// create hql query
+			Group group = session.get(Group.class, id);
+
+			// return result
+			return group;
+		} finally {
+			session.close();
+		}
+	}
+
+	@SuppressWarnings("unchecked")
+	public Group getGroupByName(String name) {
+		Session session = null;
+
+		try {
+			// get Session
+			session = hibernateutils.openSession();
+
+			// create hql query
+			Query<Group> query = session.createQuery("FROM Group WHERE name = :nameParameter");
+			
+			
+			// set Parameter
+			query.setParameter("nameParameter", name);
+			
+			// return result
+			Group group = query.uniqueResult();
+			return group;
+		} finally {
+			session.close();
+		}
+	}
+	
+	public void createGroup (Group group) {
+		Session session = null;
+
+		try {
+			// get Session
+			session = hibernateutils.openSession();
+
+			// create
+			session.save(group);
+		} finally {
+			session.close();
+		}
+	}
+	
+	public void updateGroup (Integer id, String newName) {
+		Session session = null;
+
+		try {
+			// get Session
+			session = hibernateutils.openSession();
+			session.beginTransaction();
+
+			// get Group
+			Group group = (Group) session.load(Group.class, id);
+			
+			
+			// update
+			group.setName(newName);
+			session.getTransaction().commit();
+		} finally {
+			session.close();
+		}
+	}
+	
+	public void deleteGroup (Integer id) {
+		Session session = null;
+
+		try {
+			// get Session
+			session = hibernateutils.openSession();
+			session.beginTransaction();
+
+			// get Group
+			Group group = (Group) session.load(Group.class, id);
+			
+			
+			// delete
+			session.delete(group);
+			session.getTransaction().commit();
 		} finally {
 			session.close();
 		}
